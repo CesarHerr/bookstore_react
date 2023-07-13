839b8108d16935d45c0420680c1ea1c1e8394485
Cesar Herrera <cesarherr321@gmail.com>
2023-07-13 00:14:51 -0400

    Add route to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './routes/error-page';
+import Home from './components/Home';
+import Categories from './components/Categories';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/categories',
+        element: <Categories />,
+      },
+    ],
   },
 ]);
 
---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Welcome</h1>;
+}
+
+export default Home;
