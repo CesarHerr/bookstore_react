6c789df4959b08a1f4e375717fe69b393e09332c
Cesar Herrera <cesarherr321@gmail.com>
2023-07-18 14:38:38 -0400

    Fix linters errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import  bookReducer from './books/booksSlice.js';
-import categoriesReducer from './categories/categoriesSlice.js'
+import bookReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
