2f1a0c667f2c3b10f376670a96753816a1d0e0f0
Cesar Herrera <cesarherr321@gmail.com>
2023-07-18 21:12:42 -0400

    Add remove and add buttons to the project

--- src/components/Newbook.js
+++ src/components/Newbook.js
@@ -1,10 +1,57 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { add } from '../redux/books/booksSlice';
+
 function NewBook() {
+  const [newTitle, setNewTitle] = useState('');
+  const [newAuthor, setNewAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const getTitle = (e) => {
+    setNewTitle(e.target.value);
+  };
+
+  const getAuthor = (e) => {
+    setNewAuthor(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (newTitle.trim()) {
+      const newBook = {
+        item_id: uuidv4(),
+        title: newTitle,
+        author: newAuthor,
+        category: 'Fiction',
+      };
+      dispatch(add(newBook));
+      setNewTitle('');
+      setNewAuthor('');
+    }
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Book Title" required />
-      <input type="text" placeholder="Author" required />
-      <button type="button">ADD BOOK</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={getTitle}
+          type="text"
+          placeholder="Book Title"
+          value={newTitle}
+          required
+        />
+        <input
+          onChange={getAuthor}
+          type="text"
+          placeholder="Author"
+          value={newAuthor}
+          required
+        />
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </>
   );
 }
 
