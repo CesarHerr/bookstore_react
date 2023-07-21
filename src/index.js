26e32cd6c7a2756d6fff83e8a3e2354ae3dbdbc9
Cesar Herrera <cesarherr321@gmail.com>
2023-07-20 23:09:32 -0400

    Add Sass and and reset styles

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <section className="appContainer">
+      <div className="panelBook">
+        <App />
+      </div>
+    </section>
   </Provider>,
 );
 
